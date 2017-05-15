728d76ee97c24efce555fae9b98499dc9f42e6cb
Jasper <jasper@wearekonzept.nl>
2017-05-15 11:45:51 +0200

    Part 2 of 2 Finished loading and saving to xml. Changed constructor for player. Save happens on closing of the form.

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -44,21 +44,45 @@ namespace Engine
 
                 playerData.LoadXml(xmlPlayerdata);
 
-                int currentHitPoints = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/CurrentHitPoints"));
-                int maximumHitPoints = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/MaximumHitPoints"));
-                int gold = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/Gold"));
-                int experiencePoints = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/ExperiencePoints"));
+                int currentHitPoints = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/CurrentHitPoints").InnerText);
+                int maximumHitPoints = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/MaximumHitPoints").InnerText);
+                int gold = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/Gold").InnerText);
+                int experiencePoints = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/ExperiencePoints").InnerText);
 
                 Player player = new Player(currentHitPoints, maximumHitPoints, gold, experiencePoints);
 
-                int currentLocation = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/CurrentLocation"));
-                player.CurrentLocation = World.LocationByID(currentLocation);
+                int currentLocationID = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/CurrentLocation").InnerText);
+                player.CurrentLocation = World.LocationByID(currentLocationID);
 
                 foreach (XmlNode node in playerData.SelectNodes("/Player/Stats/InventoryItems/InventoryItem"))
                 {
-                    int id = Convert.ToInt32(node.Attributes["ID"])
+                    int id = Convert.ToInt32(node.Attributes["ID"].Value);
+                    int quantity = Convert.ToInt32(node.Attributes["Quantity"].Value);
+
+                    for (int i = 0; i < quantity; i++)
+                    {
+                        player.AddItemToInventory(World.ItemByID(id));
+                    }
                 }
 
+                foreach (XmlNode node in playerData.SelectNodes("/Player/PlayerQuests/PlayerQuest"))
+                {
+                    int id = Convert.ToInt32(node.Attributes["ID"].Value);
+                    bool isCompleted = Convert.ToBoolean(node.Attributes["IsCompleted"].Value);
+
+                    PlayerQuest playerQuest = new PlayerQuest(World.QuestByID(id));
+                    playerQuest.IsCompleted = isCompleted;
+
+                    player.Quests.Add(playerQuest);
+                }
+
+                return player;
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = ex.Message;
+                
+                return Player.CreateDefaultPlayer();
             }
         }
 
@@ -167,7 +191,7 @@ namespace Engine
             stats.AppendChild(experiencePoints);
 
             XmlNode currentLocation = playerData.CreateElement("CurrentLocation");
-            currentLocation.AppendChild(playerData.CreateTextNode(this.CurrentLocation.ToString()));
+            currentLocation.AppendChild(playerData.CreateTextNode(this.CurrentLocation.ID.ToString()));
             stats.AppendChild(currentLocation);
 
             XmlNode inventoryItems = playerData.CreateElement("InventoryItems");
